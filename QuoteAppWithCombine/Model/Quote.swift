985fb29ddc65485ac5268554badb240601393c43
Kwame <kwameagyenimboateng64@gmail.com>
2022-07-31 15:32:23 +0000

    quote model

---
+++ QuoteAppWithCombine/Model/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuoteAppWithCombine
+//
+//  Created by Kwame Agyenim - Boateng on 26/07/2022.
+//
+
+import Foundation
+
+struct Quote: Decodable{
+    var content: String?
+    var author: String?
+}
